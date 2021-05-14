d70548de00b836af9d69e19d5317e610df20084e
Ioana Cristea <ioanac977@gmail.com>
2021-05-14 10:16:53 +0300

    Create controllers and services for patient

---
+++ HospitalManagement/HospitalManagement/Controllers/PatientsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HospitalManagement.Data;
+using HospitalManagement.Models;
+using HospitalManagement.Services;
+
+namespace HospitalManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        private readonly PatientService patientService;
+
+        public PatientsController(HospitalContext context)
+        {
+            patientService = new PatientService(context);
+            
+        }
+
+        // GET: api/Patients
+        [HttpGet]
+        public IEnumerable<Patient> GetPatients()
+        {
+            return  patientService.GetPatients();
+        }
+
+        // GET: api/Patients/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPatient([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var patient = await  patientService.GetPatient(id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(patient);
+        }
+
+        // PUT: api/Patients/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPatient([FromRoute] int id, [FromBody] Patient patient)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != patient.Id)
+            {
+                return BadRequest();
+            }
+
+            patientService.SetEntityState(patient, EntityState.Modified);
+
+            try
+            {
+                await patientService.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!patientService.PatientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Patients
+        [HttpPost]
+        public async Task<IActionResult> PostPatient([FromBody] Patient patient)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            patientService.AddPatient(patient);
+            await patientService.SaveChangesAsync();
+
+            return CreatedAtAction("GetPatient", new { id = patient.Id }, patient);
+        }
+
+        // DELETE: api/Patients/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePatient([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var patient = await patientService.GetPatient(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            patientService.DeletePatient(patient);
+            await patientService.SaveChangesAsync();
+
+            return Ok(patient);
+        }
+
+    }
+}
---
+++ HospitalManagement/HospitalManagement/Services/PatientService.cs
@@ -0,0 +1,57 @@
+﻿using HospitalManagement.Data;
+using HospitalManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace HospitalManagement.Services
+{
+    public class PatientService
+    {
+        private readonly HospitalContext _context;
+
+        public PatientService(HospitalContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Patient> GetPatients()
+        {
+            return _context.Patients;
+        }
+        public async Task<Patient> GetPatient([FromRoute] int id)
+        {
+
+            return await _context.Patients.FindAsync(id);
+
+        }
+        public void AddPatient(Patient patient)
+        {
+            _context.Patients.Add(patient);
+        }
+
+        public void DeletePatient(Patient patient)
+        {
+            _context.Patients.Remove(patient);
+        }
+
+        public void SetEntityState(Patient patient, EntityState entityState)
+        {
+            _context.Entry(patient).State = entityState;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+           await _context.SaveChangesAsync();
+        }
+
+        public bool PatientExists(int id)
+        {
+           return _context.Patients.Any(e => e.Id == id);
+        }
+        //_context.Patients.FindAsync(id);
+    }
+}
